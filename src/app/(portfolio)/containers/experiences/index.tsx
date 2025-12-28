6d957f0abd851362bfae3073270329c0293d4b73
viriyadhika <viriya.dhika0@gmail.com>
2025-12-28 16:47:01 -0500

    change resume

--- src/app/(portfolio)/containers/experiences/index.tsx
+++ src/app/(portfolio)/containers/experiences/index.tsx
@@ -9,14 +9,9 @@ const jobs: JobProps[] = [
     image: "gic",
     date: "Aug 2024 - Present",
     descriptions: [
-      `Built ETL pipelines and CI/CD deployment scripts to centralize financial instruments data processing from
-multiple sources using Amazon Web Services and Java Spring Boot, deployed on Kubernetes, and used by major
-downstream applications company-wide`,
-      `Enhanced a highly concurrent FastAPI Python application for data reconciliation to maintain data quality across
-100+ tables, reducing runtime of data quality scripts from 3 days to 4 hours per run, enabling checks to run
-multiple times daily instead of weekly.`,
-      `Developed a Python tool to parse requirement specifications and generate boilerplate code, improving accuracy
-and saving developer hours.`,
+      `Built ETL pipelines and CI/CD deployment scripts to centralize financial instruments data processing from multiple sources using Amazon Web Services and Java Spring Boot, deployed on Kubernetes, and used by major downstream applications company-wide.`,
+      `Enhanced a highly concurrent FastAPI Python application for data reconciliation to maintain data quality across 100+ tables, reducing runtime of data quality scripts from 3 days to 4 hours per run, enabling checks to run multiple times daily instead of weekly.`,
+      `Developed a Python tool to parse requirement specifications and automate code generation.`,
       `Developed a full-stack web application as an internal tool for production support.`,
     ],
     skills: [
@@ -60,11 +55,10 @@ and saving developer hours.`,
     image: "tiktok",
     date: "Jul 2023 - Aug 2024",
     descriptions: [
-      `Created a React webpage within the TikTok App, enabling creators to track their TikTok Shop affiliate performance, accessed by millions of monthly active users.`,
-      `Designed and implemented new data dashboards in shop.tiktok.com, partner.tiktokshop.com, and affiliate.tiktok.com. Integrated content diagnostic features in the TikTok Shop seller app by providing creators with content inspiration.`,
-      `As the front-end in charge for partner.tiktokshop.com data dashboard, reviewed pull requests, enabled greyscale release through CI/CD script, and addressed periodic user feedback, achieving high user growth in new markets.`,
-      `Leveraged internal build tools and infrastructure to support multi-region deployments. Utilized front-end data collection frameworks and Apache Hive interface to develop production monitoring dashboards.`,
-      `Optimize a smooth close to native app experience by using animation library such as Framer motion`,
+      `Created and maintained a React webpage embedded in TikTok App used by millions of monthly active users.`,
+      `Created new data dashboards and integrated diagnosis features in shop.tiktok.com, partner.tiktokshop.com and affiliate.tiktok.com. As the frontend in charge for partner data dashboard, reviewed pull requests by other contributors, improved CI/CD automation and actively addressed user feedback to the platform.`,
+      `Learnt and utilize TikTok internal tooling, build tools and infrastructure as well as different frontend development and Micro-Frontends frameworks to perform scalable deployment and production monitoring on a massive scale to multiple regions and countries.`,
+      `Utilized front-end data collection frameworks and Apache Hive to develop production monitoring dashboard and support frontend A/B testing.`,
     ],
     skills: [
       {
@@ -95,10 +89,10 @@ and saving developer hours.`,
     image: "DBS",
     date: "Jul 2021 - Jul 2023",
     descriptions: [
-      `Leveraged Camunda with Java Spring Boot to orchestrate workflows and manage subsystem API calls in a nationwide branch banking system designed for bank tellers and managers.`,
-      `Integrated the Teller Assist Unit physical device with the application, using REST API to trigger device actions, RabbitMQ and WebSocket for real-time event communication.`,
-      `Advocated for reusable front-end components by gathering cross-team requirements and maintaining widely used components, reducing development time for common components from one week to one day.`,
-      `Built testable front-end code using Jest and React Testing Library and back-end code with JUnit and Mockito, achieving 98% unit test line coverage as measured by SonarQube static code analysis.`,
+      `In charge of building a web application for several bank transactions like fixed deposit, cash deposit and withdrawal as part of the BranchConnect project. BranchConnect is a web application to be used by bank tellers to perform transaction in DBS branches in Singapore.`,
+      `Solely responsible for integration of Teller Assist Unit physical device to application using REST API to trigger device actions and event listener to receive events and show corresponding UI.`,
+      `Advocate on the usage of reusable front-end components and creating it by communicating with other teams for requirements gathering and maintaining some of the most widely used components throughout BranchConnect. Some examples are Customer Account Search, General Ledger and Cash Denomination user interface.`,
+      `Build testable front-end code by using jest and react-testing-library, performing above KPI (80%) with 98% of unit test line coverage score via Sonarqube static code analyzer.`,
       `From times to times, performing tech lead duties such as code review, distribute workload to team members, act as technical representative in software design meetings and production support.`,
       `Serve as a mentor to new software developers in the team.`,
     ],
